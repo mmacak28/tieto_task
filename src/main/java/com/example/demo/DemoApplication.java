06d722909a5b40820da6f997b53dc902e309e4c7
Mato <469393@mail.muni.cz>
2021-05-13 09:17:00 +0200

    Stop work on parser

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,25 +2,27 @@ package com.example.demo;
 
 //import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.integration.syslog.RFC5424SyslogParser;
+import com.github.palindromicity.syslog.SyslogParser;
+import com.github.palindromicity.syslog.SyslogParserBuilder;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.Reader;
+import java.util.Map;
 import java.util.Scanner;
 
 //@SpringBootApplication
-public class DemoApplication extends RFC5424SyslogParser{
+public class DemoApplication{
 
     public static void main(String[] args) {
         //SpringApplication.run(DemoApplication.class, args);
-        RFC5424SyslogParser mineParser = new RFC5424SyslogParser();
+        SyslogParser parser = new SyslogParserBuilder().build();
         try {
             Scanner input = new Scanner(new File(args[0]));
             while (input.hasNextLine()) {
                 String line = (input.nextLine());
-                Reader reader = new Reader(line);
-                Object date = mineParser.getTimestamp(reader);
-                System.out.println(date.toString());
+                Map<String,Object> syslogMap = parser.parseLine(line);
+                System.out.println(syslogMap.get("syslog.header.timestamp"));
             }
             input.close();
         }
